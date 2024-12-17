2eeaac88a8796f18f4aa79c98f495154ef3041b9
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-17 19:41:21 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -4,3 +4,5 @@ BOT_TOKEN = "7502020526:AAHGAIk6yBS0TL2J1wOpd_-mFN1HorgVc1s"  # Replace with you
 
 API_ID = "22250562"  # your api id
 API_HASH = "07754d3bdc27193318ae5f6e6c8016af"  # your api hash
+DEFAULT_THUMBNAIL = "https://envs.sh/Rdy.jpg"
+DEFAULT_PREFIX = "@ClawMoviez"
