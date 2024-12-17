f13d6cc752437167b8d701a55e8f1bf3fb45dc8c
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-17 20:16:26 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,8 +1,16 @@
-# config.py
+# bot/config.py
 
 BOT_TOKEN = "7502020526:AAHGAIk6yBS0TL2J1wOpd_-mFN1HorgVc1s"  # Replace with your bot token from @BotFather
 
 API_ID = "22250562"  # your api id
 API_HASH = "07754d3bdc27193318ae5f6e6c8016af"  # your api hash
-DEFAULT_THUMBNAIL = "https://envs.sh/Rdy.jpg"
-DEFAULT_PREFIX = "@ClawMoviez"
+
+DEFAULT_THUMBNAIL = "https://envs.sh/Rdy.jpg"  # Default thumbnail URL
+DEFAULT_PREFIX = "@ClawMoviez"  # Default prefix for filenames
+
+# File handling settings
+MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024  # 2 GB now
+CHUNK_SIZE = 2 * 1024 * 1024  # 2 MB initial chunk size
+MAX_RETRIES = 3
+RETRY_DELAY = 5
+MAX_FILE_PARTS = 3000
