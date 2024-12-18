1ecd3af11ebc558bbede358a372840a1976b99dc
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-18 13:20:52 +0530

    Update settings_handlers.py

--- bot/settings_handlers.py
+++ bot/settings_handlers.py
@@ -1,5 +1,5 @@
 from telethon import events, Button
-from bot.utils import get_user_settings, set_user_setting
+from bot.utils import get_user_settings, set_user_setting, upload_thumb
 import logging
 import os
 from telethon.tl.functions.messages import SendMediaRequest
@@ -63,3 +63,48 @@ async def remove_rule_callback_handler(event):
 async def done_settings_handler(event):
     await event.answer("Settings saved!")
     await event.delete()
+
+@events.register(events.NewMessage(func=lambda e: hasattr(e, 'client') and hasattr(e.client, 'task_data')))
+async def process_settings_input(event):
+    user_id = event.sender_id
+    task_data = event.client.task_data.get(str(user_id), {})
+    status = task_data.get("status")
+
+    if status == "set_thumbnail":
+            if event.media:
+                if isinstance(event.media, types.MessageMediaPhoto):
+                    try:
+                        file = await event.client.download_media(event.media)
+                        thumb_file_id = await upload_thumb(event,user_id, file)
+                        set_user_setting(user_id, "thumbnail", thumb_file_id)
+                        await event.respond("Thumbnail updated!")
+                        if os.path.exists(file):
+                            os.remove(file)
+                    except Exception as e:
+                        logging.error(f"Error in process_settings_input set_thumbnail: {e}")
+                        await event.respond("Error updating thumbnail. Please try again later")
+                else:
+                    await event.respond("Please send a valid image for the thumbnail.")
+            else:
+                await event.respond("Please send a valid image for the thumbnail.")
+            del event.client.task_data[str(user_id)]
+            await settings_handler(event)
+
+    elif status == "set_prefix":
+            new_prefix = event.text.strip()
+            set_user_setting(user_id, "prefix", new_prefix)
+            await event.respond(f"Prefix updated to: {new_prefix}")
+            del event.client.task_data[str(user_id)]
+            await settings_handler(event)
+
+    elif status == "add_rename_rule":
+            rule = event.text.strip()
+            user_settings = get_user_settings(user_id)
+            if rule not in user_settings["rename_rules"]:
+                user_settings["rename_rules"].append(rule)
+                set_user_setting(user_id, "rename_rules", user_settings["rename_rules"])
+                await event.respond(f"Added rename rule: {rule}")
+            else:
+                await event.respond(f"Rule already exists: {rule}")
+            del event.client.task_data[str(user_id)]
+            await settings_handler(event)
