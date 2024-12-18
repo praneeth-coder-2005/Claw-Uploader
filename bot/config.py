1e660e2b4c9a5c5ac604baa413fc5fd94ba92598
praneeth-coder-2005 <savediskofc@gmail.com>
2024-12-18 13:41:17 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -5,7 +5,7 @@ API_ID = "22250562"  # Replace with your API ID
 API_HASH = "07754d3bdc27193318ae5f6e6c8016af"  # Replace with your API Hash
 
 DEFAULT_THUMBNAIL = "https://envs.sh/Rdy.jpg"  # Default thumbnail URL (can be changed in settings)
-DEFAULT_PREFIX = "@ClawMoviez"  # Default prefix for filenames (can be changed in settings)
+DEFAULT_PREFIX = "@ClawMoviez - "  # Default prefix for filenames (can be changed in settings)
 
 # File handling settings (for downloads and uploads)
 MAX_FILE_SIZE = 2 * 1024 * 1024 * 1024  # 2 GB
